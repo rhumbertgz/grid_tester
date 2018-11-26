70564bf2afe009df2f766d6b6db502c65ca38b01
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2018-11-26 23:40:24 +0100

    Add restart function

--- lib/grid_tester.ex
+++ lib/grid_tester.ex
@@ -22,6 +22,10 @@ defmodule GridMonitor.Tester do
     GenServer.cast(:tester, :remove_node)
   end
 
+  def restart do
+    GenServer.cast(:tester, :restart)
+  end
+
   @impl true
   def init(_args) do
     Logger.debug("#{@module}.init")
@@ -31,7 +35,7 @@ defmodule GridMonitor.Tester do
   @impl true
   def handle_cast(:add_node, state) do
     Logger.debug("#{@module}.add_node")
-    broker = { :broker, :'grid_broker@169.254.195.175'}
+    broker = { :broker, :'grid_broker@192.168.1.8'}
     data = %{id: 2, tmin: 19, tmax: 23, insulation: 500.0, powerUsage: 2000, troomData: [19.0], tambientData: [8.5], hpData: [300], ehpData: [200]}
     GenServer.cast(broker, {:add_node, data})
     data = %{id: 3, tmin: 20, tmax: 23, insulation: 25.0, powerUsage: 2080, troomData: [19.0], tambientData: [8.5], hpData: [300], ehpData: [200]}
@@ -44,7 +48,7 @@ defmodule GridMonitor.Tester do
   @impl true
   def handle_cast(:update_node, state) do
     Logger.debug("#{@module}.update_node")
-    broker = { :broker, :'grid_broker@169.254.195.175'}
+    broker = { :broker, :'grid_broker@192.168.1.8'}
     data = %{id: 2,  powerUsage: 2500, troom: 20, tambient: 18, hp: 350, ehp: 250}
     GenServer.cast(broker, {:update_node, data})
     data = %{id: 2,  powerUsage: 2550, troom: 18, tambient: 19, hp: 380, ehp: 280}
@@ -56,12 +60,21 @@ defmodule GridMonitor.Tester do
 
   def handle_cast(:remove_node, state) do
     Logger.debug("#{@module}.remove_node")
-    broker = { :broker, :'grid_broker@169.254.195.175'}
+    broker = { :broker, :'grid_broker@192.168.1.8'}
     data = %{id: 2}
     GenServer.cast(broker, {:remove_node, data})
 
     {:noreply, state}
   end
+
+  def handle_cast(:restart, state) do
+    Logger.debug("#{@module}.reset")
+    broker = { :broker, :'grid_broker@192.168.1.8'}
+    GenServer.cast(broker, :restart)
+
+    {:noreply, state}
+  end
+
 
 
 
